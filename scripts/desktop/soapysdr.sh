d9fa2aaf79ae1cc59f8b816c32495dfb41ec98e2
André Moraes <andrelom@gmail.com>
2025-06-06 20:55:13 -0300

    Create soapysdr.sh

---
+++ scripts/desktop/soapysdr.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+echo "Installing dependencies..."
+
+sudo apt update
+
+sudo apt install -y cmake g++ git libpython3-dev python3-numpy swig
+
+echo "Building and installing SoapySDR from source..."
+
+git clone https://github.com/pothosware/SoapySDR.git
+
+cd SoapySDR
+
+mkdir -p build && cd build
+
+cmake ..
+
+make -j$(nproc)
+
+sudo make install
+
+sudo ldconfig
+
+echo "To verify the installation, run the following command:"
+echo "SoapySDRUtil --info"
+echo "You can also run the following command to list available devices:"
+echo "SoapySDRUtil --find"
+
+echo "Installation complete!"
