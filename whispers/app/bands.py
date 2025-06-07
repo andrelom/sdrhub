6789634cda54055030eac2fb3fa5c0ea331a50fe
André Moraes <andrelom@gmail.com>
2025-06-07 00:30:38 -0300

    Update bands.py

--- whispers/app/bands.py
+++ whispers/app/bands.py
@@ -1,3 +1,8 @@
+import numpy as np
+
+def generate_band_range(start_mhz, end_mhz, step_khz):
+    return list(np.arange(start_mhz * 1e6, end_mhz * 1e6 + 1, step_khz * 1e3))
+
 BAND_CONFIGS = {
     "hf": {
         "frequencies": [3.7e6, 7.1e6, 14.2e6, 21.3e6],
@@ -6,7 +11,7 @@ BAND_CONFIGS = {
         "output_dir": "recordings_hf",
     },
     "vhf": {
-        "frequencies": [145.5e6, 146.52e6],
+        "frequencies": generate_band_range(144, 148, 25),  # de 144 MHz a 148 MHz
         "sample_rate": 240000,
         "demod": "fm",
         "output_dir": "recordings_vhf",
